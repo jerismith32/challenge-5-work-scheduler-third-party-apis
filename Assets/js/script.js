67afe5bffa8052627e2637c053dc57d396144a1d
jerismith32 <jerismith32@gmail.com>
2021-05-16 11:45:24 -0400

    updated highlighting feature

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
     $("#currentDay").text(moment().format("MMM Do YY"));
-    console.log(CurrentDay);
+    var currenttime = moment().hours();
+
+    $( "textarea" ).each(function() {
+        console.log(typeof $( this ).attr("time") );
+        console.log('time', typeof currenttime)
+        if (currenttime === parseInt($( this ).attr("time"))  ) {
+            $(this).addClass("present");
+        }
+        else if (currenttime > parseInt($( this ).attr("time"))  ) {
+            $(this).addClass("past");
+        }
+        else if (currenttime < parseInt($( this ).attr("time"))  ) {
+            $(this).addClass("future");
+        }
+      });
 })
 
 
